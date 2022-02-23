07e0118023280a60ad419415b750b6c893401c2d
nicholasjg97 <nicholas.j.gaytan97@gmail.com>
2022-02-22 19:53:10 -0600

    new challenges

---
+++ src/questionaire.js
@@ -0,0 +1,25 @@
+// What are the first 4 characters of an if statement?
+// if (
+
+// What is the last line of an if statement?
+// }
+
+// if (city = "Las Vegas") {
+// In the above statement, what character is incorrect?
+// =
+
+// Code the first line of an if statement that tests whether one variable has the same value as another.
+// if (a === b) {
+
+// Code the first line of an if statement that tests whether a variable has the value of a particular string.
+// if (a === "b") {
+
+// Code an if statement that tests whether pCode is "10010". If so, assign to the variable city the string "New York". city has already been declared.
+// if (pCode === "10010") {
+//   city = "New York";
+// }
+
+// Code an if statement. Test whether a variable has a particular numerical value. If so, assign a new value to that variable, as in x = 1;
+// if (anything === 1) {
+//   anything = 2;
+// }
